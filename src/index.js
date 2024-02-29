fd910d1e4f7dfa3a738c2cabfff22a5207ae6ef5
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-29 21:42:13 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+//axios.defaults.headers.common["x-api-key"] = 'api_key=live_oLlF3i9CosWMP9pQL5psZHR7F8jMmBHEDuHoIiQwiJ8RAR3MtiVmqBGOn2oFUQ3y';
+
+const breedSelect = document.querySelector('.breed-select');
+
+breedSelect.addEventListener('click', () => {
+try {
+    
+} catch (error) {
+    
+}
+}
+);
