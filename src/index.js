061ce8384651fceec95df4d0d3ebf9f2cb55da9c
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-01 19:52:46 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import axios from "axios";
 //axios.defaults.headers.common["x-api-key"] = 'api_key=live_oLlF3i9CosWMP9pQL5psZHR7F8jMmBHEDuHoIiQwiJ8RAR3MtiVmqBGOn2oFUQ3y';
 
-const breedSelect = document.querySelector('.breed-select');
+import axios from "axios";
 
-breedSelect.addEventListener('click', () => {
-try {
-    
-} catch (error) {
-    
+const fetchBreedsBtn = document.querySelector(".btn");
+
+fetchBreedsBtn.addEventListener("click", async () => {
+  try {
+    const breeds = await fetchBreeds();
+    console.log(breeds)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function fetchBreeds() {
+ const response = await axios.get(
+   `https://api.thecatapi.com/v1/breeds`);
+  return response.data;
 }
+
+function renderBreeds(breeds) {
+  const markup = breeds
+    .map(({ id, title, body, userId }) => {
+      return `<li>
+          <h2 class="post-title">${title.slice(0, 30)}</h2>
+          <p><b>Post id</b>: ${id}</p>
+          <p><b>Author id</b>: ${userId}</p>
+          <p class="post-body">${body}</p>
+        </li>`;
+    })
+    .join("");
+  breedList.insertAdjacentHTML("beforeend", markup);
 }
-);
